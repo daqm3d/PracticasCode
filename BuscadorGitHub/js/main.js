5659680676597ee1bef8bb497dd5e2df1c4e7744
daqm3d <daqm3d@gmail.com>
2024-09-10 11:07:28 -0400

    add js y img carpetas

---
+++ BuscadorGitHub/js/main.js
@@ -0,0 +1,7 @@
+const API = 'https://api.github.com';
+// https://api.github.com/users/ para buscar usuarios
+// https://api.github.com/search/users?q=daqm3
+
+// https://api.github.com/search/repositories?q=daqm3d busca por nombre de repo
+
+// "https://api.github.com/search/repositories?q=topic:machine-learning" buscar por etiqueta
